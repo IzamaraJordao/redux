94974ed107ac090a8f4d8febdc2ef4a3cdde9ecf
IzamaraJordao <89167184+IzamaraJordao@users.noreply.github.com>
2022-04-28 22:21:50 -0300

    master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.css'
+import Card from "./components/Card";
+import Intervalo from "./components/Intervalo";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div className="App"> 
+      <h1> Projeto Redux </h1>
+
+      <div className="linha">
+       <Intervalo></Intervalo>
+        
+      </div>
+
+      <div className="linha">
+        <Card title="Card 2" green> teste 2</Card>
+        <Card title="Card 3" blue> teste 3</Card>
+        <Card title="Card 4" purple> teste 4</Card>
+      </div>
     </div>
-  );
+  )
 }
+
+
 
 export default App;
